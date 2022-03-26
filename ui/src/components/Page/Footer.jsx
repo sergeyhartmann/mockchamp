eadee9fc806d0e8bbc57eb318e61ea4acbc3f8e0
Sergey Shevelev <sergeyhartmann@yandex.com>
2022-03-26 20:39:57 +0300

    Show build version in web interface

--- ui/src/components/Page/Footer.jsx
+++ ui/src/components/Page/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Grid, Link, Typography } from '@mui/material';
-import { GITHUB_LINK, VERSION } from '../../consts';
 
 const Footer = () => {
     return (
         <Grid container justifyContent="center" spacing={2} mt={6}>
             <Grid item>
                 <Typography variant="body2" color="textSecondary">
-                    MockChamp build ver. {VERSION}
+                    MockChamp build ver. {process.env.REACT_APP_VERSION || 'DEV'}
                 </Typography>
             </Grid>
 
@@ -18,7 +17,12 @@ const Footer = () => {
             </Grid>
 
             <Grid item>
-                <Link href={GITHUB_LINK} target="_blank" variant="body2" color="textSecondary">
+                <Link
+                    href="https://github.com/sergeyhartmann/mockchamp"
+                    target="_blank"
+                    variant="body2"
+                    color="textSecondary"
+                >
                     GitHub
                 </Link>
             </Grid>
--- ui/src/consts.js
+++
@@ -1,2 +0,0 @@
-export const VERSION = '1.3.0';
-export const GITHUB_LINK = 'https://github.com/sergeyhartmann/mockchamp';
